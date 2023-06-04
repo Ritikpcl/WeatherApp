d907aca85c2347fc9acbdae8cc74d133f88aa96c
Ritik Panchal <ritikpcl@gmail.com>
2023-06-04 14:00:28 +0530

    edit url

--- client/src/api/weatherApi.js
+++ client/src/api/weatherApi.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "weather-app-ashy-pi.vercel.app"});
+const API = axios.create({baseURL: "https://weather-nbrqknt4e-ritikpcl.vercel.app/"});
 
 export const getWeather = (query) => API.get(`/weather/search/${query}`)

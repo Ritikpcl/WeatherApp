f0ad31baee3e52e0ad0b86420dd93134e9ea07c9
Ritik Panchal <ritikpcl@gmail.com>
2023-06-04 13:21:58 +0530

    add vercel url

--- client/src/api/weatherApi.js
+++ client/src/api/weatherApi.js
@@ -1,5 +1,8 @@
 import axios from "axios";
+// import path from "path"
+import dotenv from "dotenv"
+dotenv.config();
 
-const API = axios.create({baseURL : "http://localhost:5000"});
+const API = axios.create({baseURL: process.env.BASE_URL });
 
 export const getWeather = (query) => API.get(`/weather/search/${query}`)
